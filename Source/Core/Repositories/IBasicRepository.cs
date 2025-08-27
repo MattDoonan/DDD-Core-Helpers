e2edf644a7943efcc2d90c6c42bb58c89ba6770e
MattDoonan <matt.j.doonan@gmail.com>
2025-08-27 18:23:07 +1200

    IBasicRepositoryAsync

--- Source/Core/Repositories/IBasicRepository.cs
+++ Source/Core/Repositories/IBasicRepository.cs
@@ -6,3 +6,7 @@ namespace Core.Repositories;
 public interface IBasicRepository<in TId, T> : IBasicAddRepository<T>, IBasicUpdateRepository<T>, IBasicGetRepository<TId, T>
     where T : IAggregateRoot<TId>
     where TId : IAggregateRootId;
+    
+public interface IBasicRepositoryAsync<in TId, T> : IBasicAddRepositoryAsync<T>, IBasicUpdateRepositoryAsync<T>, IBasicGetRepositoryAsync<TId, T>
+    where T : IAggregateRoot<TId>
+    where TId : IAggregateRootId;
